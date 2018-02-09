8cdad1bd4c2f40cde7ceb6b64c3513edb8d020a3
Samir Ali Chaouche <samir.ali.chaouche@gmail.com>
2018-02-09 11:19:02 -0800

    added src / compute_cosines

---
+++ src/compute_cosines.py
@@ -0,0 +1,20 @@
+import urllib.request
+import csv
+import codecs
+import math
+
+url = 'http://rapid-hub.org/data/angles_UCI_CS.csv'
+data_stream = urllib.request.urlopen(url)
+csv_reader = csv.reader(codecs.iterdecode(data_stream,'utf-8'))
+for row in csv_reader:
+    print(row)
+##next(csv_reader)
+##iddeg = []
+##for row in csv_reader:
+##    station_id = int(row[0])
+##    deg = int(row[1])
+##    iddeg.append((station_id,deg,math.cos(deg)))
+##
+##for row in iddeg:
+##    print('station_id: {}, angle_degrees: {}, cosine value: {}'.format(row[0], row[1], row[2]))
+##
