085233a5379199cb684acb996a3f8831a341bf64
Samir Ali Chaouche <samir.ali.chaouche@gmail.com>
2018-02-24 18:15:34 -0800

    changed wget to url

--- tst/tst_dwnl.sh
+++ tst/tst_dwnl.sh
@@ -3,5 +3,5 @@
 INPUTCSV=http://rapid-hub.org/data/angles_UCI_CS.csv
 OUTPUTCSV=http://rapid-hub.org/data/angles_UCI_CS_out.csv
 
-wget -O angles_UCI_CS.csv $INPUTCSV
-wget -O angles_UCI_CS_out.csv $OUTPUTCSV
+curl -fsS -o angles_UCI_CS.csv $INPUTCSV
+curl -fsS -o angles_UCI_CS_out.csv $OUTPUTCSV
